fd440a9d9acc6c53e76889c8e9e8bc7292a92c0b
shubhin151 <60954538+shubh-151@users.noreply.github.com>
2024-06-22 12:01:43 +0530

    Nav Bar code push with some classes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
+"use client";
+import React, { useState } from "react";
+import Link from "next/link";
+import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
+import { cn } from "@/utils/cn";
 
-const Navbar = () => {
+const Navbar = ({ className }: { className?: string }) => {
+    const [active, setActive] = useState<string | null>(null);
   return (
-    <div>
-      Navbar
-    </div>
+    <div
+      className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
+    >
+        Navbar
+        </div>
   )
 }
 
