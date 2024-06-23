2b8a64698300d7b840bb23ced45358bea7f6c155
shubhin151 <60954538+shubh-151@users.noreply.github.com>
2024-06-23 09:43:43 +0530

    Hero section update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
+import HeroSection from "@/components/HeroSection";
 
 
 export default function Home() {
   return (
     
         <main className="min-h-screen antialised bg-grid-white/[0.02]">
-          <h1 className="text-2xl text-center">Chai aur Code</h1>
+          <HeroSection/>
         </main>
       
   );
