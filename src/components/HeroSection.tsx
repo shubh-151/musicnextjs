992199632b5a439e961182c8d2a2ec9c594e13fe
shubhin151 <60954538+shubh-151@users.noreply.github.com>
2024-06-24 22:02:12 +0530

    hero updated

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -12,7 +12,7 @@ const HeroSection = () => {
       />
       <div className="p-4 relative z-10 w-full text-center">
         <h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">
-          ka ho coca pila kya haal chaal
+          Shreya tripahi
 
         </h1>
         <p className="mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto">
