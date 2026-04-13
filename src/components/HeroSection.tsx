14a1eddc47db973e16f7598bc7e173e59c1f027f
Shubham Shukla <60954538+shubh-151@users.noreply.github.com>
2026-04-13 14:03:19 +0530

    Update HeroSection title from 'Master the Art of Music' to 'Hello baby'

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,4 +1,4 @@
-import Link from "next/link";
+oimport Link from "next/link";
 import React from "react";
 import { Button } from "./ui/moving-border";
 import { Spotlight } from "./ui/Spotlight";
@@ -12,7 +12,7 @@ const HeroSection = () => {
       />
       <div className="p-4 relative z-10 w-full text-center">
         <h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">
-          Master the Art of Music
+          Hello baby
 
         </h1>
         <p className="mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto">
